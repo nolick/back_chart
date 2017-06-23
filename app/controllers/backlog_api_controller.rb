15d631860bc7a49ae56d71ef6d99842881789b5e
nsaitou <n.saito@infocom-east.co.jp>
2017-06-23 15:48:01 +0900

    add backlog_kit sample

--- app/controllers/backlog_api_controller.rb
+++ app/controllers/backlog_api_controller.rb
@@ -1,2 +1,13 @@
 class BacklogApiController < ApplicationController
+  require 'backlog_kit'
+
+  def index
+    client = BacklogKit::Client.new(
+      space_id: 'rebecca',
+      api_key: 'KR3zcYkiA0mdoeKuIEICfYaYxvVwAiX7xEtLh9GjLQnv4MJDkbdYCgpkibf9sROQ',
+    )
+    render json: client.get_versions(projectIdOrKey: 39284)
+    versions = client.get('projects/39284/versions')
+    versions.body.select {|b| b.name == "2017_08_02"}
+  end
 end
