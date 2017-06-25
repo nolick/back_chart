b80496c26ef1dd18ef02de1d21a95e9cd24c10a9
Norihiko Saito <nsaito@NorihikonoMacBook-Air.local>
2017-06-26 08:17:45 +0900

    [WIP]

--- app/controllers/backlog_api_controller.rb
+++ app/controllers/backlog_api_controller.rb
@@ -1,13 +1,32 @@
 class BacklogApiController < ApplicationController
   require 'backlog_kit'
+  before_action :intialize_backlog_client
 
-  def index
-    client = BacklogKit::Client.new(
+  def milestones
+    milestones = @client.get('projects/39284/versions')
+    render json: milestones
+  end
+
+  def issues
+    milestone = params[:milestone] || '2017_07_05'
+    issues = @client.get_issues.body.select do |i|
+      if i.milestone.present?
+        i.milestone.first.name == milestone
+      else
+        false
+      end
+    end
+    render json: issues
+  end
+
+  private
+
+  #TODO: isolated from controller by service class
+  # https://www.sitepoint.com/ddd-for-rails-developers-part-1-layered-architecture/
+  def intialize_backlog_client
+    @client ||= BacklogKit::Client.new(
       space_id: 'rebecca',
-      api_key: 'KR3zcYkiA0mdoeKuIEICfYaYxvVwAiX7xEtLh9GjLQnv4MJDkbdYCgpkibf9sROQ',
+      api_key: '',
     )
-    render json: client.get_versions(projectIdOrKey: 39284)
-    versions = client.get('projects/39284/versions')
-    versions.body.select {|b| b.name == "2017_08_02"}
   end
 end
